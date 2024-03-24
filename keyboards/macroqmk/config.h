1bc7f2b92cfca65dbe025a173266161351c2476d
Max Aniskovich <m.a.aniskovich@gmail.com>
2024-03-24 13:39:10 +0300

    Add MacroQMK

---
+++ keyboards/macroqmk/config.h
@@ -0,0 +1,15 @@
+// Copyright 2023 m-a-aniskovich (@m-a-aniskovich)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define OLED_UPDATE_PROCESS_LIMIT 5
+
+#define ENCODER_DIRECTION_FLIP
+#define ENCODER_RESOLUTION 2
+
+/* disable action features */
+#define NO_ACTION_TAPPING
+#define NO_ACTION_ONESHOT
+
+//#define NO_ACTION_LAYER
